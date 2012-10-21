9a745260e96858f44bc7dbb070c5704ecd767e5f
Tomasz Mloduchowski <q@qdot.me>
2012-10-21 14:46:22 +0000

    more declarations

--- QESCPOS/qescpos.h
+++ QESCPOS/qescpos.h
@@ -9,6 +9,8 @@
 
 class QESCPOSSHARED_EXPORT QESCPOS : public QextSerialPort
 {
+    Q_OBJECT
+    Q_PROPERTY(int underline /*READ underline*/ WRITE setUnderline)
 public:
     QESCPOS();
     QESCPOS(const QString &name);
@@ -16,10 +18,80 @@ public:
     enum Type {
         TM_T88IV
     };
+    
+    enum ICS {
+        ICS_USA = 0,
+        ICS_FRANCE = 1,
+        ICS_GERMANY = 2,
+        ICS_UK = 3,
+        ICS_DENMARK1 = 4,
+        ICS_SWEDEN = 5,
+        ICS_ITALY = 6,
+        ICS_SPAIN = 7,
+        ICS_JAPAN = 8,
+        ICS_NORWAY = 9,
+        ICS_DENMARK2 = 10,
+        ICS_SPAIN2 = 11,
+        ICS_LATINAMERICA = 12,
+        ICS_KOREAN = 13,
+        ICS_SLOVENIA = 14,
+        ICS_CHINESE = 15,
+    };
+    
+    enum CCT {
+        CCT_PC437 = 0,
+        CCT_KATAKANA = 1,
+        CCT_PC850 = 2,
+        CCT_PC860 = 3,
+        CCT_PC863 = 4,
+        CCT_PC865 = 5,
+        CCT_KANJI1 = 6,
+        CCT_KANJI2 = 7,
+        CCT_KANJI3 = 8,
+        CCT_WPC1252 = 16,
+        CCT_PC866 = 17,
+        CCT_PC852 = 18,
+        CCT_PC858 = 19,
+        CCT_PAGE254 = 254,
+        CCT_PAGE255 = 255,
+    };
+    
+    enum Color {
+        C_NONE   = 48,
+        C_COLOR1 = 49,
+        C_COLOR2 = 50,
+        C_COLOR3 = 51,
+    };
+    
+    enum Just {
+        JUST_LEFT = 0,
+        JUST_CENTER = 1,
+        JUST_RIGHT = 2,
+    };
+    
 
     void setType(Type t);
 
-    DECLARE_STATIC(cutPaper, bool full=false, int pos=-1)
+    DECLARE_STATIC(cutPaper,        bool full=false, int pos=-1)
+    DECLARE_STATIC(setUnderline,    int  thickness=1)
+    DECLARE_STATIC(setEmphasized,   bool emphasized=true)
+    DECLARE_STATIC(setDoubleStrike, bool doubleStrike=true)
+    DECLARE_STATIC(setFont,         int  font=0)
+    DECLARE_STATIC(setInternationalCharacterSet, ICS ics=ICS_UK)
+    DECLARE_STATIC(setClockwiseRotation, int rotation=1)
+    DECLARE_STATIC(setCharacterCodeTable, CCT cct=CCT_PC437)
+    DECLARE_STATIC(setUpsideDown,   bool upsideDown=true)
+    DECLARE_STATIC(setCharacterColor,  Color color = C_COLOR2)
+    DECLARE_STATIC(setBackgroundColor, Color color = C_COLOR2)
+    DECLARE_STATIC(setShadingColor,    Color color = C_COLOR2, bool shadow = true)
+    DECLARE_STATIC(setCharacterSize,   int width = 2, int height = 2)
+    DECLARE_STATIC(setReverse,         bool reverse   = true)
+    DECLARE_STATIC(setSmoothing,       bool smoothing = true)
+    
+    
+    DECLARE_STATIC(setJustification,   Just just = JUST_CENTER)
+    DECLARE_STATIC(printRaster,        QBitmap b, int scaleX = 0, int scaleY = 1)
+    
 };
 
 #endif // QESCPOS_H

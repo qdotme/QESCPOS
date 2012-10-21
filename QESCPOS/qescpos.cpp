d40bdbe7430420f4986b68cf9bdc7bc77e3aa8a7
Tomasz Mloduchowski <q@qdot.me>
2012-10-21 14:52:38 +0000

    cut paper command - more work on it

--- QESCPOS/qescpos.cpp
+++ QESCPOS/qescpos.cpp
@@ -13,8 +13,8 @@ QESCPOS::QESCPOS(const QString &name) : QextSerialPort(name)
 QByteArray QESCPOS::cutPaperCommand(bool full, int pos) {
     QByteArray ret(GS"V");
     if (pos < 0) // function A
-        return ret.append((char)full);
-
+        return ret.append((unsigned char)full);
+    return ret.append((unsigned char)((int)full+66)).append((unsigned char)pos)
 
     //! @todo: support for GS V function C, D
 }
